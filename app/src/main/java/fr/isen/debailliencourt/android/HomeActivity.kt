a7cef38fb21c2998fdacd39bdbeee8999cc29577
evanndev <evann.de-bailliencourt@isen.yncrea.fr>
2020-01-20 20:42:20 +0100

    Changement HomeAcitvity->CycleVie

--- app/src/main/java/fr/isen/debailliencourt/android/HomeActivity.kt
+++ app/src/main/java/fr/isen/debailliencourt/android/HomeActivity.kt
@@ -1,12 +1,29 @@
 package fr.isen.debailliencourt.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        val cycleIntent : Intent =  Intent(this,CycleVie::class.java)
+
+        imCycleVie.setOnClickListener{
+            startActivity(cycleIntent)
+        }
+
+        imRCycleVie.setOnClickListener{
+            startActivity(cycleIntent)
+        }
+
+        textCycleVie.setOnClickListener{
+            startActivity(cycleIntent)
+        }
     }
 }
