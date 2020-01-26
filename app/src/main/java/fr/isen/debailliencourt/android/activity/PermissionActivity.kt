46b9381a0660bc159d8830698b97854da8912d1d
evanndev <evann.de-bailliencourt@isen.yncrea.fr>
2020-01-26 18:02:55 +0100

    add picturePerm/change pict_Perm

--- app/src/main/java/fr/isen/debailliencourt/android/activity/PermissionActivity.kt
+++ app/src/main/java/fr/isen/debailliencourt/android/activity/PermissionActivity.kt
@@ -2,6 +2,7 @@ package fr.isen.debailliencourt.android.activity
 
 import android.Manifest
 import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -11,30 +12,73 @@ import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import fr.isen.debailliencourt.android.R
+import kotlinx.android.synthetic.main.activity_permissions.*
 
 class PermissionActivity : AppCompatActivity() {
     companion object{
+        //camera request code
         private const val REQUEST_CAMERA: Int  = 1
+        //image pick code
+        private const val IMAGE_PICK_CODE = 1000;
+        //Permission code
+        private const val PERMISSION_CODE = 1001;
+    }
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permissions)
 
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-            // Permissions Granted
-
-            Toast.makeText(this, "Permissions Granted", Toast.LENGTH_SHORT).show()
-        }
-        else{
-            //Permissions hasn't been granted
-
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
-                Toast.makeText(this, "Camera permission is needed, trust me", Toast.LENGTH_SHORT).show()
+        pictGalleryPerm.setOnClickListener{
+            //system OS is > Marshmallow
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE)
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery()
+                }
             }
-            val permissionsList = arrayOf("Manifest.permission.Camera")
-            requestPermissions(permissionsList, REQUEST_CAMERA)
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery()
+            }
+        }
+    }
+    //handle requested permission result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    pickImageFromGallery()
+                }
+                else{
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission refusée", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            pictGalleryPerm.setImageURI(data?.data)
         }
     }
 }
